52e6d885e4e1caf61bcd0c2a062a0080cc8165fe
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-03-06 23:43:51 -0800

    Solving Stack & Queue 1Q

---
+++ test.js
@@ -0,0 +1,13 @@
+let person = [
+    {name: "John",
+    age: 30,
+    city: "New York"},
+    {name: "Aung",
+    age: 32,
+    city: "Myanmar"}
+    
+]
+
+for(key of person) {
+    console.log(key);
+}
